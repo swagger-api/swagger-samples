76fa15672aacd5a268e4aad0762ae771a2267ccb
Tony Tam <fehguy@gmail.com>
2015-06-06 21:53:42 -0700

    updated title

--- java/java-jersey-jaxrs/src/main/java/io/swagger/sample/Bootstrap.java
+++ java/java-jersey-jaxrs/src/main/java/io/swagger/sample/Bootstrap.java
@@ -13,7 +13,7 @@ public class Bootstrap extends HttpServlet {
   @Override
   public void init(ServletConfig config) throws ServletException {
     Info info = new Info()
-      .title("Swagger Sample App")
+      .title("Swagger Petstore")
       .description("This is a sample server Petstore server.  You can find out more about Swagger " + 
         "at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, " +
         "you can use the api key `special-key` to test the authorization filters.")
