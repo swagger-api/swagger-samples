1611ab93f23063f969f8caf5593f7eaaffd8b4bf
mar612 <mar612@mailinator.com>
2015-07-17 13:53:51 -0400

    Update PetResource.java
    Fixed the allowableValues to match the getPetById's notes

--- java/java-jersey2/src/main/java/io/swagger/sample/resource/PetResource.java
+++ java/java-jersey2/src/main/java/io/swagger/sample/resource/PetResource.java
@@ -41,7 +41,7 @@ public class PetResource {
   @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid ID supplied"),
       @ApiResponse(code = 404, message = "Pet not found") })
   public Response getPetById(
-      @ApiParam(value = "ID of pet that needs to be fetched", allowableValues = "range[1,5]", required = true) @PathParam("petId") String petId)
+      @ApiParam(value = "ID of pet that needs to be fetched", allowableValues = "range[1,10]", required = true) @PathParam("petId") String petId)
       throws io.swagger.sample.exception.NotFoundException {
     Pet pet = petData.getPetbyId(ru.getLong(0, 100000, 0, petId));
     if (null != pet) {
