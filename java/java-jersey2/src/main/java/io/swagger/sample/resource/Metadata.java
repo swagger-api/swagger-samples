8eea146cd5f3388cd463c65fc0e6c7030cbb849c
Tony Tam <fehguy@gmail.com>
2017-06-27 22:04:59 -0700

    enabled yaml

--- java/java-jersey2/src/main/java/io/swagger/sample/resource/Metadata.java
+++ java/java-jersey2/src/main/java/io/swagger/sample/resource/Metadata.java
@@ -4,5 +4,6 @@ package io.swagger.sample.resource;
 import io.swagger.oas.annotations.info.Info;
 
 @Info(title = "My API", version = "1.2.3", description = "A sample API")
+
 public class Metadata {
 }
