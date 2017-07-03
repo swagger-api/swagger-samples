95addcea6afdc529ee88c696a0cc04a041c96a46
Ron <webron@gmail.com>
2017-07-03 09:26:18 -0700

    added missing import

--- java/java-jersey2/src/main/java/io/swagger/sample/resource/PetResource.java
+++ java/java-jersey2/src/main/java/io/swagger/sample/resource/PetResource.java
@@ -31,6 +31,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
