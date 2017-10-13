c9339dbfb4f277b8e38dab6fa83aedc2faaae1b4
frantuma <frantuma@yahoo.com>
2017-10-13 15:07:41 +0200

    fix info annotation

--- java/java-jersey2/src/main/java/io/swagger/sample/resource/Metadata.java
+++ java/java-jersey2/src/main/java/io/swagger/sample/resource/Metadata.java
@@ -1,9 +1,9 @@
 package io.swagger.sample.resource;
 
 
+import io.swagger.oas.annotations.OpenAPIDefinition;
 import io.swagger.oas.annotations.info.Info;
 
-@Info(title = "My API", version = "1.2.3", description = "A sample API")
-
+@OpenAPIDefinition(info = @Info(title = "My API", version = "1.2.3", description = "A sample API"))
 public class Metadata {
 }
