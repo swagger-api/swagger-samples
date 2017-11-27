65ad90b148b5e4c825de58f39fc0468cc65486b8
gracekarina <gracekarina.gonzalez@gmail.com>
2017-11-27 16:57:57 -0500

    springboot-jersey sample

---
+++ java/inflector-springboot-jersey/src/test/java/io/swagger/samples/inflector/springboot/InflectorApplicationTests.java
@@ -0,0 +1,16 @@
+package io.swagger.samples.inflector.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InflectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
