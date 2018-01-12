35b67e169c30db67f5c4c8d23ec598c0d2c118cb
frantuma <frantuma@yahoo.com>
2018-01-12 09:17:51 +0100

    refs swagger-api/swagger-core#2607 - adds sub resource resource

--- java/java-jersey2-configfile/src/main/java/io/swagger/sample/MyApplication.java
+++ java/java-jersey2-configfile/src/main/java/io/swagger/sample/MyApplication.java
@@ -3,12 +3,23 @@ package io.swagger.sample;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/sample")
 public class MyApplication extends ResourceConfig {
+//public class MyApplication extends Application {
 
     public MyApplication() {
     //public MyApplication(@Context ServletConfig servletConfig) {
         super();
+        /*
+         * It seems that Jersey (at least 2.26) incorrectly registers also non root resources (available via getClasses())
+         * even if stated otherwise in https://jersey.github.io/documentation/latest/deployment.html
+         * 4.7.2.3.2. JAX-RS application with a custom Application subclass
+         * this could result in non existing operations being added if served by subresource classes.
+         *
+         * To avoid this behaviour provide own packages/classes/resources/singleton here
+         */
+        super.packages("io.swagger.sample.resource", "io.swagger.v3.jaxrs2.integration.resources");
     }
 }
