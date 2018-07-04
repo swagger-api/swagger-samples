ab69bb5a15dc6070092c562d431838fc530f2988
Rafael López <ralphdoe12@gmail.com>
2018-07-03 20:57:18 -0500

    XML Support

--- java/swagger-petstore/src/main/java/io/swagger/petstore/utils/Util.java
+++ java/swagger-petstore/src/main/java/io/swagger/petstore/utils/Util.java
@@ -6,13 +6,20 @@ import javax.ws.rs.core.MediaType;
 
 public class Util {
 
-    public static MediaType getMediaType(RequestContext request) {
+    private static final String APPLICATION = "application";
+    private static final String YAML = "yaml";
+
+    public static MediaType getMediaType(final RequestContext request) {
         MediaType outputType = MediaType.APPLICATION_JSON_TYPE;
+
+        if (request.getMediaType().equals(MediaType.APPLICATION_ATOM_XML_TYPE)) {
+            return request.getMediaType();
+        }
 
         boolean isJsonOK = false;
         boolean isYamlOK = false;
 
-        final MediaType yamlMediaType = new MediaType("application", "yaml");
+        final MediaType yamlMediaType = new MediaType(APPLICATION, YAML);
 
         for (final MediaType mediaType : request.getAcceptableMediaTypes()) {
             if (mediaType.equals(MediaType.APPLICATION_JSON_TYPE)) {
