df78e2053070780216c98ac6e8b2ed47991becf8
frantuma <frantuma@yahoo.com>
2019-04-12 14:05:13 +0200

    expose not fully resolved petstore v3 spec

--- java/swagger-petstore/src/main/java/io/swagger/petstore/utils/HandleAuthUrlProcessor.java
+++ java/swagger-petstore/src/main/java/io/swagger/petstore/utils/HandleAuthUrlProcessor.java
@@ -1,13 +1,44 @@
 package io.swagger.petstore.utils;
 
 import io.swagger.oas.inflector.config.OpenAPIProcessor;
+import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.net.URL;
 
 public class HandleAuthUrlProcessor implements OpenAPIProcessor {
 
     @Override
     public void process(OpenAPI openAPI) {
+        OpenAPI petstoreOpenAPI = null;
+        try {
+            // try to load from resources
+            URL url = HandleAuthUrlProcessor.class.getClassLoader().getResource("openapi-full.yaml");
+            if(url != null) {
+                try {
+                    petstoreOpenAPI = Yaml.mapper().readValue(new File(url.getFile()), OpenAPI.class);
+                } catch (Exception e) {
+                    // continue
+                }
+            }
+        } catch (Exception e) {
+            // continue
+        }
+
+        if (petstoreOpenAPI != null) {
+            openAPI
+                    .components(petstoreOpenAPI.getComponents())
+                    .extensions(petstoreOpenAPI.getExtensions())
+                    .info(petstoreOpenAPI.getInfo())
+                    .paths(petstoreOpenAPI.getPaths())
+                    .security(petstoreOpenAPI.getSecurity())
+                    .externalDocs(petstoreOpenAPI.getExternalDocs())
+                    .servers(petstoreOpenAPI.getServers())
+                    .tags(petstoreOpenAPI.getTags());
+        }
+
         String oauthHost = System.getenv("SWAGGER_OAUTH_HOST");
         if (StringUtils.isBlank(oauthHost)) {
             return;
